cedc4d3db408a54b864614bd9d2b794a20d84a4a
Janosch Püschel <janosch.pueschel@gmail.com>
2022-11-14 16:56:37 +0100

    added function highlight

--- script.js
+++ script.js
@@ -3,46 +3,19 @@ let homeScore = 0;
 function addOneHome() {
   homeScore += 1;
   document.getElementById("home_score").textContent = homeScore;
-  if (homeScore === guestScore) {
-    document.getElementById("home_score").className = "score--loser";
-    document.getElementById("guest_score").className = "score--loser";
-  } else if (homeScore > guestScore) {
-    document.getElementById("home_score").className = "score--winner";
-    document.getElementById("guest_score").className = "score--loser";
-  } else if (homeScore < guestScore) {
-    document.getElementById("home_score").className = "score--loser";
-    document.getElementById("guest_score").className = "score--winner";
-  }
+  highlight();
 }
 
 function addTwoHome() {
   homeScore += 2;
   document.getElementById("home_score").textContent = homeScore;
-  if (homeScore === guestScore) {
-    document.getElementById("home_score").className = "score--loser";
-    document.getElementById("guest_score").className = "score--loser";
-  } else if (homeScore > guestScore) {
-    document.getElementById("home_score").className = "score--winner";
-    document.getElementById("guest_score").className = "score--loser";
-  } else if (homeScore < guestScore) {
-    document.getElementById("home_score").className = "score--loser";
-    document.getElementById("guest_score").className = "score--winner";
-  }
+  highlight();
 }
 
 function addThreeHome() {
   homeScore += 3;
   document.getElementById("home_score").textContent = homeScore;
-  if (homeScore === guestScore) {
-    document.getElementById("home_score").className = "score--loser";
-    document.getElementById("guest_score").className = "score--loser";
-  } else if (homeScore > guestScore) {
-    document.getElementById("home_score").className = "score--winner";
-    document.getElementById("guest_score").className = "score--loser";
-  } else if (homeScore < guestScore) {
-    document.getElementById("home_score").className = "score--loser";
-    document.getElementById("guest_score").className = "score--winner";
-  }
+  highlight();
 }
 
 let guestScore = 0;
@@ -50,46 +23,19 @@ let guestScore = 0;
 function addOneGuest() {
   guestScore += 1;
   document.getElementById("guest_score").textContent = guestScore;
-  if (homeScore === guestScore) {
-    document.getElementById("home_score").className = "score--loser";
-    document.getElementById("guest_score").className = "score--loser";
-  } else if (homeScore > guestScore) {
-    document.getElementById("home_score").className = "score--winner";
-    document.getElementById("guest_score").className = "score--loser";
-  } else if (homeScore < guestScore) {
-    document.getElementById("home_score").className = "score--loser";
-    document.getElementById("guest_score").className = "score--winner";
-  }
+  highlight();
 }
 
 function addTwoGuest() {
   guestScore += 2;
   document.getElementById("guest_score").textContent = guestScore;
-  if (homeScore === guestScore) {
-    document.getElementById("home_score").className = "score--loser";
-    document.getElementById("guest_score").className = "score--loser";
-  } else if (homeScore > guestScore) {
-    document.getElementById("home_score").className = "score--winner";
-    document.getElementById("guest_score").className = "score--loser";
-  } else if (homeScore < guestScore) {
-    document.getElementById("home_score").className = "score--loser";
-    document.getElementById("guest_score").className = "score--winner";
-  }
+  highlight();
 }
 
 function addThreeGuest() {
   guestScore += 3;
   document.getElementById("guest_score").textContent = guestScore;
-  if (homeScore === guestScore) {
-    document.getElementById("home_score").className = "score--loser";
-    document.getElementById("guest_score").className = "score--loser";
-  } else if (homeScore > guestScore) {
-    document.getElementById("home_score").className = "score--winner";
-    document.getElementById("guest_score").className = "score--loser";
-  } else if (homeScore < guestScore) {
-    document.getElementById("home_score").className = "score--loser";
-    document.getElementById("guest_score").className = "score--winner";
-  }
+  highlight();
 }
 
 function reset() {
@@ -99,4 +45,17 @@ function reset() {
   document.getElementById("guest_score").textContent = guestScore;
   document.getElementById("home_score").className = "score--loser";
   document.getElementById("guest_score").className = "score--loser";
+}
+
+function highlight() {
+  if (homeScore === guestScore) {
+    document.getElementById("home_score").className = "score--loser";
+    document.getElementById("guest_score").className = "score--loser";
+  } else if (homeScore > guestScore) {
+    document.getElementById("home_score").className = "score--winner";
+    document.getElementById("guest_score").className = "score--loser";
+  } else if (homeScore < guestScore) {
+    document.getElementById("home_score").className = "score--loser";
+    document.getElementById("guest_score").className = "score--winner";
+  }
 }
